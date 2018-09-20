362c623b9f4b1256c0fcb884f98408069eeabebe
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2018-09-20 17:27:01 +0100

    json fixed and stable for good

--- V.4 GET RTC AS VARIABLE FOR TIME STAMP/SIM800_HTTP-POST/SIM800_HTTP-POST.ino
+++ V.4 GET RTC AS VARIABLE FOR TIME STAMP/SIM800_HTTP-POST/SIM800_HTTP-POST.ino
@@ -1,5 +1,5 @@
 //For more Information visit www.aeq-web.com?ref=arduinoide
-
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
@@ -9,6 +9,7 @@ String apn_u = "";                     //APN-Username
 String apn_p = "";                    //APN-Password
 String url = "http://halogenapp.pythonanywhere.com/iot";  //URL for HTTP-POST-REQUEST
 String json;   //String for the json Paramter (e.g. jason)
+String time_stamp = "juo"; //String for time_stamp collection 
 unsigned long   CO_Value = 0; //CO MQ7 sensor value holder MQ7
 unsigned long   CH4_Value = 0; //H2 MQ5 sensor value holder
 unsigned long   AQ_Value = 0; // MQ135 AIR QUALITY sensor value holder
@@ -32,64 +33,52 @@ void setup() {
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
   delay(10000);
-
-  //get realtime VALUE from network
-//  mySerial.println("AT+CLTS=1");
-//  runsl();
-//  delay(100);
-//  mySerial.println("AT+CLTS?");
-//  runsl();
-//  delay(100);
-//  mySerial.println("AT&W");
-//  runsl();
-//  delay(100);
-//  mySerial.println("AT+CCLK?");
-//  runsl();
-//  delay(100);
 }
 
 void loop() { // run over and over
+//      json = "{\"co\":" +  String(avg_CO) + ",\"ch4\":" +  String(avg_CH4) + ",\"aq\":" +  String(avg_AQ) + ",\"h\":" +  String(avg_H) +"}";
 
-    json = "{\"co\":" +  String(avg_CO) + ",\"ch4\":" +  String(avg_CH4) + ",\"aq\":" +  String(avg_AQ) + ",\"h\":" +  String(avg_H) +"}";
+//      json = "{\"co\":" +  String(avg_CO) + ",\"ch4\":" +  String(avg_CH4) + ",\"aq\":" +  String(avg_AQ) + ",\"h\":" +  String(avg_H) + ",\"time\":" +  "\"String(time_stamp)\""  +"}";
+//      json = "{\"co\":" +  String(avg_CO) + ",\"ch4\":" +  String(avg_CH4) + ",\"aq\":" +  String(avg_AQ) + ",\"h\":" +  String(avg_H) + ",\"time\":" + time_stamp +"}";
 
-      
-        getSensorData();
-//get average sensor val for the minute
-
-//    if (count == 270){ //270/60secs equals 4mins and 30secs. 30secs extra is given for the sending of the post request to makke a total of 5min interval per data post
-    if (count == 60){
-        avg_CO = CO_Value / count ;
-        avg_CH4 = CH4_Value / count ;
-        avg_AQ = AQ_Value / count ;
-        avg_H = H_Value / count ;
-        
-      Serial.println("Turning off sensor Heaters ....." );
-      delay(200); //200milisecs
-      Serial.println("SENDING AVERAGE READINGS TO SERVER " );
-
-      digitalWrite(relay_pin, HIGH);
-      gsm_sendhttp(); 
-
-      //Start the GSM-Modul and start the transmisson
-
-      Serial.print("CO =  ");
-      Serial.print(avg_CO, DEC);               // prints the value read CO
-      Serial.print(" PPM  ");
-    
-      Serial.print("METHANE =  ");
-      Serial.print(avg_CH4, DEC);               // prints the value read CH4
-      Serial.print(" PPM   ");
-    
-      Serial.print("AQ =  ");
-      Serial.print(avg_AQ, DEC);               // prints the value read AIR QUALITY
-      Serial.print(" PPM   ");
-    
-      Serial.print("HYDROGEN =  ");
-      Serial.print(avg_H, DEC);               // prints the value read HYDROGEN
-      Serial.println(" PPM");
-      
-      reset_sensor_values();
-    };
+      getSensorData(); //get average sensor val for the minute
+      json_();
+      Serial.println(json);
+      Serial.println(count);
+      // if (count == 270){ //270/60secs equals 4mins and 30secs. 30secs extra is given for the sending of the post request to makke a total of 5min interval per data post
+      if (count == 60){
+                avg_CO = CO_Value / count ;
+                avg_CH4 = CH4_Value / count ;
+                avg_AQ = AQ_Value / count ;
+                avg_H = H_Value / count ;
+                
+                Serial.println("Turning off sensor Heaters ....." );
+                delay(200); //200milisecs
+                Serial.println("SENDING AVERAGE READINGS TO SERVER " );
+          
+                digitalWrite(relay_pin, HIGH);
+                gsm_sendhttp(); 
+          
+                //Start the GSM-Modul and start the transmisson
+          
+                Serial.print("CO =  ");
+                Serial.print(avg_CO, DEC);               // prints the value read CO
+                Serial.print(" PPM  ");
+              
+                Serial.print("METHANE =  ");
+                Serial.print(avg_CH4, DEC);               // prints the value read CH4
+                Serial.print(" PPM   ");
+              
+                Serial.print("AQ =  ");
+                Serial.print(avg_AQ, DEC);               // prints the value read AIR QUALITY
+                Serial.print(" PPM   ");
+              
+                Serial.print("HYDROGEN =  ");
+                Serial.print(avg_H, DEC);               // prints the value read HYDROGEN
+                Serial.println(" PPM");
+                
+                reset_sensor_values();
+              };
    
     delay(1000); //Wait 6 minute
 }
@@ -97,9 +86,6 @@ void loop() { // run over and over
 
 void gsm_sendhttp() {
 
-  mySerial.println("AT+CCLK?");
-  runsl();
-  delay(100);
   mySerial.println("AT");
   runsl();//Print GSM Status an the Serial Output;
   delay(4000);
@@ -108,14 +94,6 @@ void gsm_sendhttp() {
   delay(100);
   mySerial.println("AT+SAPBR=3,1,APN," + apn);
   runsl();
-  
-//  delay(100);
-//  mySerial.println("AT+SAPBR=3,1,USER," + apn_u); //Comment out, if you need username
-//  runsl();
-//  delay(100);
-//  mySerial.println("AT+SAPBR=3,1,PWD," + apn_p); //Comment out, if you need password
-//  runsl();
-
   delay(100);
   mySerial.println("AT+SAPBR =1,1");
   runsl();
@@ -171,13 +149,7 @@ void getSensorData(){
   CH4_Value = CH4_Value + ch4_val;
   AQ_Value = AQ_Value + aq_val;
   H_Value = H_Value + h_val;
-
   
-//  Serial.print("CO " + String(CO_Value) + "--" +  String(co_val));
-//  Serial.print("  CH4 " + String(CH4_Value) + "--" +  String(ch4_val)); 
-//  Serial.print("  AQ " + String(AQ_Value)+ "--" +  String(aq_val)); 
-//  Serial.print("  H " + String(H_Value) + "--" +  String(h_val));
-//  Serial.println("  COUNT " + String(count));
   count = count + 1;
 }
 
@@ -193,10 +165,35 @@ void reset_sensor_values(){
 }
 
 
+String set_time_stamp() {
+    mySerial.println("AT+CCLK?");//GET TIME
+    runsl();
+    delay(100);
+//
+//    time_stamp = mySerial.readString();
+//    time_stamp.replace("+CCLK:", "");
+//    time_stamp.replace("OK", "");
+//    Serial.println(time_stamp);
+//    Serial.println(json);
+     return 0;
+};
 
 
+void json_(){
+ StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["co"] = avg_CO;
+  root["ch4"] = avg_CH4;
+  root["aq"] = avg_AQ;
+  root["h"] = avg_H;
+  root["time"] = time_stamp;
+  String place_holder;
+  root.printTo(place_holder);
 
-
+  json = place_holder;
+//  root.prettyPrintTo(Serial);
+//  json = Serial.readString();
+};
 
 
 
@@ -212,3 +209,30 @@ void reset_sensor_values(){
 //  mySerial.println("AT&W"); //SAVE NEW SETTINGS SO THAT SIM800L MODULE SETS TIME ON STARTUP SUBSEQUENTLY
 //  runsl();
 //  delay(100);
+
+
+  //get realtime VALUE from network
+//  mySerial.println("AT+CLTS=1");
+//  runsl();
+//  delay(100);
+//  mySerial.println("AT+CLTS?");
+//  runsl();
+//  delay(100);
+//  mySerial.println("AT&W");
+//  runsl();
+//  delay(100);
+//  mySerial.println("AT+CCLK?");
+//  runsl();
+//  delay(100);
+
+//  mySerial.println("AT+SAPBR=3,1,USER," + apn_u); //Comment out, if you need username
+//  runsl();
+//  delay(100);
+//  mySerial.println("AT+SAPBR=3,1,PWD," + apn_p); //Comment out, if you need password
+//  runsl();
+
+//  Serial.print("CO " + String(CO_Value) + "--" +  String(co_val));
+//  Serial.print("  CH4 " + String(CH4_Value) + "--" +  String(ch4_val)); 
+//  Serial.print("  AQ " + String(AQ_Value)+ "--" +  String(aq_val)); 
+//  Serial.print("  H " + String(H_Value) + "--" +  String(h_val));
+//  Serial.println("  COUNT " + String(count));
